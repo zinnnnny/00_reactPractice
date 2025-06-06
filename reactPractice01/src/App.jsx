75f4eb8e7e08bc0162a44bfaab39fbeadfe7cd5d
zinnnnny <jieun489@gmail.com>
2025-06-06 23:08:32 +0900

    250606
    react Practice

---
+++ reactPractice01/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>안녕하세요</div>
+    </>
+  )
+}
+
+export default App
